ca9b1ea2a79778106f30f011f2f3f02873d73db9
Samuel Hammill <shammill88@gmail.com>
2018-05-11 11:37:48 +1000

    Test + Clean Up

---
+++ csharp/DataStructures.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+
+public class DataStructures
+{
+	public void DataStructures
+	{}
+	
+	
+	public void ListImplementation()
+	{
+		// Basic new List
+		List<string> newStringList = new List<String>();
+		
+		// New List with default values
+		List<string> newStringList = new List<String> {"string1", "hello", "world"};
+	}
+}
