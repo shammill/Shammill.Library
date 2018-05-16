b4115b7de3ec29c93b88ace5eed91cf141237484
Samuel Hammill <shammill88@gmail.com>
2018-05-16 10:36:14 +1000

    Added an Enums class

---
+++ SamsLibrary/csharp/Enums.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamsLibrary.csharp
+{
+    public static class Enums
+    {
+
+        public enum MyEnum
+        {
+            [Description("Employee")]
+            Employee,
+            [Description("Manager")]
+            Manager
+        }
+
+        // Get description Attribute from an Enum
+        public static string GetDescription<T>(this T e) where T : IConvertible
+        {
+            string description = null;
+
+            if (e is Enum)
+            {
+                Type type = e.GetType();
+                Array values = System.Enum.GetValues(type);
+
+                foreach (int val in values)
+                {
+                    if (val == e.ToInt32(CultureInfo.InvariantCulture))
+                    {
+                        var memberInfo = type.GetMember(e.ToString());
+                        var descriptionAttributes = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                        if (descriptionAttributes.Length > 0)
+                        {
+                            description = ((DescriptionAttribute)descriptionAttributes[0]).Description;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            return description;
+        }
+    }
+
+    // usually you'll get this from using System.ComponentModel; or using System.ComponentModel.Primatives; but that wasn't working.
+    internal class DescriptionAttribute : Attribute
+    {
+        public string Description;
+
+        public DescriptionAttribute(string v)
+        {
+            this.Description = v;
+        }
+    }
+}
