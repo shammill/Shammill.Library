4b0d4d8f2d37c5dbc929e10ee926915f00b5c5fd
Samuel Hammill <shammill88@gmail.com>
2018-05-23 14:56:48 +1000

    Added some more file functions

--- SamsLibrary/csharp/FileFunctions.cs
+++ SamsLibrary/csharp/FileFunctions.cs
@@ -21,6 +21,77 @@ namespace SamsLibrary
             return extension;
         }
 
+
+        public string GetSystemDirectory(Environment.SpecialFolder folder = Environment.SpecialFolder.MyDocuments)
+        {
+            var localData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var appData = Path.GetDirectoryName(localData);
+
+            return Environment.GetFolderPath(folder);
+        }
+
+        // Read File Line By Line
+        public static List<string> FileToStringList(string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    try
+                    {
+                        lines.Add(line);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error Parsing Person: " + line);
+                    }
+                }
+            }
+            return lines;
+        }
+
+        // Roberts way of reading the file. Not as good as mine. Bad on Memory vs Line by Line
+        public static List<string> FileToMemory(string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                
+                string file = sr.ReadToEnd();
+                var rawlines = file.Split(new String[] { Environment.NewLine }, new StringSplitOptions());
+                foreach (var line in rawlines)
+                {
+                    lines.Add(line);
+                }
+
+            }
+            return lines;
+        }
+
+
+        // also checkout File.WriteAllLines(path, linesList, Encoding.Unicode);
+        public static void OutputToFile(string outputFile, string output)
+        {
+            using (StreamWriter sw = File.AppendText(outputFile))
+            {
+                sw.WriteLine(output);
+            }
+        }
+
+        public static string GetOutputFileName(string inputFile)
+        {
+            string filePath = Path.GetDirectoryName(inputFile);
+            string fileName = Path.GetFileNameWithoutExtension(inputFile);
+            string fileExtension = Path.GetExtension(inputFile);
+            string outputFile = String.Format("{0}\\{1}{3}", filePath, fileName, fileExtension);
+
+            return outputFile;
+        }
+
     }
 }
 
