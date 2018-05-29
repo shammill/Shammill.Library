94b26d3dbe32e3794a6483846ffced024b5dc8d2
Samuel Hammill <shammill88@gmail.com>
2018-05-29 12:48:03 +1000

    String function example

--- SamsLibrary/csharp/StringFunctions.cs
+++ SamsLibrary/csharp/StringFunctions.cs
@@ -19,6 +19,13 @@ namespace SamsLibrary
             result = result.Replace("\"", @"\\x22");
             return result;
         }
+
+        public static string Reverse(this string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
     }
 
 
