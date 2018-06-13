9236c26dbe1fd0de6e8e7b3015c170ad9a6398d6
Samuel Hammill <shammill88@gmail.com>
2018-06-13 16:39:41 +1000

    Added getvaluefromdescription for enum

--- SamsLibrary/csharp/Enums.cs
+++ SamsLibrary/csharp/Enums.cs
@@ -67,6 +67,27 @@ namespace SamsLibrary.csharp
 
             return enumeration.ToString();
         }
+
+        public static T GetValueFromDescription<T>(string description)
+        {
+            var type = typeof(T);
+            if (!type.IsEnum) throw new InvalidOperationException();
+            foreach (var field in type.GetFields())
+            {
+                if (Attribute.GetCustomAttribute(field,
+                    typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+                {
+                    if (attribute.Description == description)
+                        return (T)field.GetValue(null);
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return (T)field.GetValue(null);
+                }
+            }
+            return default(T);
+        }
     }
 
     public class Enums
