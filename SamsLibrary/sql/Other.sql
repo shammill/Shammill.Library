0eaddeffa619c9f3600e592b6a1fb8a4df068966
shammill <shammill88@gmail.com>
2019-05-24 19:59:43 +1000

    Added sessions sql

---
+++ SamsLibrary/sql/Other.sql
@@ -0,0 +1,10 @@
+﻿-- find sessions
+select count(*) as sessions,
+     s.host_name,
+     s.host_process_id,
+     s.program_name,
+     db_name(s.database_id) as database_name
+from sys.dm_exec_sessions s
+where is_user_process = 1
+group by host_name, host_process_id, program_name, database_id
+order by count(*) desc;
